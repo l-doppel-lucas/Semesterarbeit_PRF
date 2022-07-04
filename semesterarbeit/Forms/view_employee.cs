b349bdbcb71b2fb61876d27195de1771096f7960
l-doppel-lucas <lucaskoeppli@hotmail.com>
2022-07-04 16:30:22 +0200

    Forms View erstellt

---
+++ semesterarbeit/Forms/view_employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace semesterarbeit.Forms
+{
+    public partial class view_employee : Form
+    {
+        public view_employee()
+        {
+            InitializeComponent();
+        }
+    }
+}
