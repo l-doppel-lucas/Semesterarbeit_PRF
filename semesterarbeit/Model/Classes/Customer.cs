fc72328aecf031a10a06959f47ba734cae6b063b
l-doppel-lucas <lucaskoeppli@hotmail.com>
2022-08-08 18:41:27 +0200

    Customer Class mit NotesHistory ergänzt // Frage an Hr. Palmer als Comment

--- semesterarbeit/Model/Classes/Customer.cs
+++ semesterarbeit/Model/Classes/Customer.cs
@@ -21,21 +21,25 @@ namespace semesterarbeit
         public string Companyadress { get; set; }
         public CustType Type { get; set; }
         public bool Companycontact { get; set; }
+        public string NotesHistory{ get; set; }
+
+        //Frage an Palmer: Mandatory Fields selbst definieren?
 
 
         /******** Constructor ********/
 
-        public Customer (bool status, int id, string title, string salutation, string firstname, string lastname, DateTime birthdate, string gender, string ahv,
+        public Customer(bool status, int id, string title, string salutation, string firstname, string lastname, DateTime birthdate, string gender, string ahv,
             string nationality, string privatephone, string mobilephone, string businessphone, string businessfax,
-            string mail, string changehistory, string companyname, string companyadress, CustType type, bool companycontact) : 
+            string mail, string changehistory, string companyname, string companyadress, CustType type, bool companycontact, string notesHistory) :
 
-            base(status, id, title, salutation, firstname, lastname, birthdate, gender, 
+            base(status, id, title, salutation, firstname, lastname, birthdate, gender,
             ahv, nationality, privatephone, mobilephone, businessphone, businessfax, mail, changehistory)
         {
             Companyname = companyname;
             Companyadress = companyadress;
             Type = type;
             Companycontact = companycontact;
+            NotesHistory = notesHistory;
         }
     }
 }
