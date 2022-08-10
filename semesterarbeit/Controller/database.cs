64128f19322bfd0b933aa5950c85351e45f4fec4
l-doppel-lucas <lucaskoeppli@hotmail.com>
2022-08-10 15:56:31 +0200

    Addes Class Database with Binding Lists and Methods for Adding and Deleting Objs

---
+++ semesterarbeit/Controller/database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace semesterarbeit.Controller
+{
+    class database
+    {
+        //Create new binding list "contactList" of type "Person"
+        //Binding list is used to refresh the datasource of the listbox
+        //Source: https://stackoverflow.com/questions/17615069/how-to-refresh-datasource-of-a-listbox
+        public BindingList<Person> contactList = new BindingList<Person>();
+
+        //Create new binding list "searchResults" of type "Person"
+        public BindingList<Person> searchResults = new BindingList<Person>();
+
+        /*************** Methods ***************/
+
+        //Method to add an object of type "Person" to the list "contactList"
+        public void AddPerson(Person p)
+        {
+            contactList.Add(p);
+        }
+
+        //Method to remove an object of the list "contactList"
+        public void DeletePerson(Person p)
+        {
+            contactList.Remove(p);
+        }
+
+    }
+}
