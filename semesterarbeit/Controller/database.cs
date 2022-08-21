ce9f2568d2559e7c5fde04f3f32ad7cf2c6969a8
l-doppel-lucas <lucaskoeppli@hotmail.com>
2022-08-21 13:28:59 +0200

    nicht verwendete Usings entfernt

--- semesterarbeit/Controller/database.cs
+++ semesterarbeit/Controller/database.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
-using System.Xml.Linq;
-using System.Xml.Serialization;
 
 namespace semesterarbeit.Controller
 {
